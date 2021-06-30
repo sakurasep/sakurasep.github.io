3138cc7a82fb14c4167a7df25cedddc20f87c7c5
sakurasep <13562008871@163.com>
2021-06-30 22:01:09 +0800

    Site updated: 2021-06-30 22:01:06

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,17 +1,1 @@
-// 浏览器搞笑标题
-var OriginTitle = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-    if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/funny.ico");
-        document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
-        clearTimeout(titleTime);
-    }
-    else {
-        $('[rel="icon"]').attr('href', "/favicon.ico");
-        document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
-        titleTime = setTimeout(function () {
-            document.title = OriginTitle;
-        }, 2000);
-    }
-});
+var titleTime,OriginTitle=document.title;document.addEventListener("visibilitychange",(function(){document.hidden?($('[rel="icon"]').attr("href","/funny.ico"),document.title="╭(°A°`)╮ 页面崩溃啦 ~",clearTimeout(titleTime)):($('[rel="icon"]').attr("href","/favicon.ico"),document.title="(ฅ>ω<*ฅ) 噫又好啦 ~"+OriginTitle,titleTime=setTimeout((function(){document.title=OriginTitle}),2e3))}));
