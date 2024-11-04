61aeb2bac0234ca0267e410f77ce39bcb8d58877
sakurasep <13562008871@163.com>
2024-11-04 21:11:37 +0800

    文章更新:动漫年鉴

---
+++ js/services/memos.js
@@ -0,0 +1,71 @@
+utils.jq(() => {
+  $(function () {
+    const els = document.getElementsByClassName('ds-memos');
+    for (var i = 0; i < els.length; i++) {
+      const el = els[i];
+      const api = el.getAttribute('api');
+      if (api == null) {
+        continue;
+      }
+      const default_avatar = el.getAttribute('avatar') || def.avatar;
+      const limit = el.getAttribute('limit');
+      const host = api.replace(/https:\/\/(.*?)\/(.*)/i, '$1');
+      // layout
+      utils.request(el, api, function(response) {
+        const data = response.memos; // 获取memos数组
+        var users = [];
+        const filter = el.getAttribute('user');
+        if (filter && filter.length > 0) {
+          users = filter.split(",");
+        }
+        var hide = [];
+        const hideStr = el.getAttribute('hide');
+        if (hideStr && hideStr.length > 0) {
+          hide = hideStr.split(",");
+        }
+        data.forEach((item, i) => {
+          if (limit && i >= limit) {
+            return;
+          }
+          if (item.creator && users.length > 0) {
+            if (!users.includes(item.creator)) {
+              return;
+            }
+          }
+          let date = new Date(item.createTime);
+          var cell = '<div class="timenode" index="' + i + '">';
+          cell += '<div class="header">';
+          if (!users.length && !hide.includes('user')) {
+            cell += '<div class="user-info">';
+            if (default_avatar.length > 0) {
+              cell += `<img src="${default_avatar}">`;
+            }
+            cell += '<span>' + '上杉九月' + '</span>';
+            cell += '</div>';
+          }
+          cell += '<span>' + date.toLocaleString() + '</span>';
+          cell += '</div>';
+          cell += '<div class="body">';
+          cell += marked.parse(item.content || '');
+          var imgs = [];
+          for (let res of item.resources) {
+            if (res.type.includes('image/')) {
+              imgs.push(res);
+            }
+          }
+          if (imgs.length > 0) {
+            cell += '<div class="tag-plugin image">';
+            for (let img of imgs) {
+              const imgPath = `https://${host}/file/${img.name}/${img.filename}`;
+              cell += `<div class="image-bg"><img src="${imgPath}" data-fancybox="memos"></div>`;
+            }
+            cell += '</div>';
+          }
+          cell += '</div>';
+          cell += '</div>';
+          $(el).append(cell);
+        });
+      });
+    }
+  });
+});
